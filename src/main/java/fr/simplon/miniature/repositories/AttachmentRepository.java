fc8fb96834587e926e403482d595e82c6ea3082c
shiipou <shiishii@nocturlab.fr>
2025-03-05 00:21:00 +0100

    feat!: Initial commit

--- src/main/java/fr/simplon/miniature/repositories/AttachmentRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.simplon.miniature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.simplon.miniature.models.Attachment;
-
-public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-}
--- src/main/java/fr/simplon/miniature/repositories/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.simplon.miniature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.simplon.miniature.models.Group;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
